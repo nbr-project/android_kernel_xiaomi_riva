f83c0a589db26f46cec37fa2681bb5e508757463
KangMamles <imzea69@gmail.com>
2021-06-11 07:36:28 +0000

    DroneCI: Format buildscript with shfmt
    Signed-off-by: KangMamles <imzea69@gmail.com>

--- build-kernel.sh
+++ build-kernel.sh
@@ -1,6 +1,6 @@
-clone () {
-git clone --depth=1 https://github.com/nbr-project/AnyKernel3 -b stock anykernel-3
-git clone --depth=1 https://github.com/nbr-project/nbr-clang -b master
+clone() {
+    git clone --depth=1 https://github.com/nbr-project/AnyKernel3 -b stock anykernel-3
+    git clone --depth=1 https://github.com/nbr-project/nbr-clang -b master
 }
 
 export TZ=Asia/Jakarta
@@ -8,27 +8,27 @@ export PATH=$(pwd)/nbr-clang/bin:$PATH
 export KBUILD_BUILD_HOST=drone
 export KBUILD_BUILD_USER=mamles
 
-compile () {
-make O=out ARCH=arm64 riva_defconfig
-make O=out ARCH=arm64 \
-    CC=clang \
-    AR=llvm-ar \
-    NM=llvm-nm \
-    OBJCOPY=llvm-objcopy \
-    OBJDUMP=llvm-objdump \
-    STRIP=llvm-strip \
-    CROSS_COMPILE=aarch64-linux-gnu- \
-    CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
-    -j$(nproc --all)
-if ! [ -a  out/arch/arm64/boot/Image.gz-dtb ]; then
-    exit 1
-fi
+compile() {
+    make O=out ARCH=arm64 riva_defconfig
+    make O=out ARCH=arm64 \
+        CC=clang \
+        AR=llvm-ar \
+        NM=llvm-nm \
+        OBJCOPY=llvm-objcopy \
+        OBJDUMP=llvm-objdump \
+        STRIP=llvm-strip \
+        CROSS_COMPILE=aarch64-linux-gnu- \
+        CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
+        -j$(nproc --all)
+    if ! [ -a out/arch/arm64/boot/Image.gz-dtb ]; then
+        exit 1
+    fi
 }
 
-repack () {
-cp out/arch/arm64/boot/Image.gz-dtb anykernel-3
-cd anykernel-3
-zip -r9q stock-kernel-riva.zip * -x .git README.md $(echo *.zip)
+repack() {
+    cp out/arch/arm64/boot/Image.gz-dtb anykernel-3
+    cd anykernel-3
+    zip -r9q stock-kernel-riva.zip * -x .git README.md $(echo *.zip)
 }
 
 clone
